99194b4c9c0e6b0d7c2ef9c93ac3742581d449cd
Dmitriy <difat1@inbox.ru>
2022-03-26 22:17:22 +0300

    Task 1

---
+++ webpack.common.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.js$/, 
+                exclude : /node_modules/, 
+                loader: 'babel-loader'
+            },
+            {
+            test: /\.css$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader'
+            ]
+        }]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin()
+    ]
+}
